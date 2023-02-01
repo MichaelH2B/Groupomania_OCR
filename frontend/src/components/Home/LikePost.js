99b77cd2752bf2fc3134dba5eee29370956faacc
Michael Schmitz <michael.schmitz@live.fr>
2023-02-01 13:31:14 +0100

    modif LikePost

--- frontend/src/components/Home/LikePost.js
+++ frontend/src/components/Home/LikePost.js
@@ -1,6 +1,63 @@
+// function LikeButton({ post }) { 
+//   const userId = localStorage.getItem('userId'); 
+//   const [arrayUsersLiked, setUserLiked] = useState(post.usersLiked); 
+//   const [liked, setLiked] = useState(arrayUsersLiked.includes(userId));  
+//   const [likedNb, setLikedNb] = useState(post.usersLiked.length);  
+
+//   const like = (e) => { 
+   
+//     axios(
+//       { 
+//         method: "post", 
+//         url: `http://localhost:5000/api/post/like/` + post._id,
+//         headers: {
+//           Authorization: localStorage.getItem('token'),
+//         },
+//       })
+//       .then((res) => {
+//         if(!liked){
+//           let temp = [...arrayUsersLiked];
+//           temp.push(userId);
+//           // console.log(temp);
+//           // console.log(userId);
+//           setUserLiked(temp);
+//           setLiked(true); 
+//         }else{  
+//           let newArray = arrayUsersLiked.filter(en => en !== userId);
+//           // console.log(arrayUsersLiked);
+//           setUserLiked(newArray);
+//           setLiked(false);  
+//         }
+//         setLikedNb(res.data.nbLike);  
+       
+        
+//       })
+//   }  
+
+//   useEffect(() => {
+//     if(post.usersLiked.length > 0){
+//       setUserLiked(post.usersLiked); 
+//     }else{
+//       setUserLiked([]);
+//     }
+//     // console.log(liked);
+//   }, [post.usersLiked]);
+ 
+
+//   return (
+//     <div className="blocLike">
+//       <div className={ `btn-like` } >
+//           <FontAwesomeIcon icon={faHeart} id={liked ? 'like' : 'unlike'} className={liked ? 'liked' : ''} type="button" onClick={(e) => like(e)} value={likedNb} />
+//       </div> 
+//       <span className="number">{likedNb}</span>
+//     </div>
+//   );
+// }
+
+// export default LikeButton;
+
 import React, { useEffect, useState } from "react";
 import axios from "axios"; 
-
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -8,10 +65,8 @@ function LikeButton({ post }) {
   const userId = localStorage.getItem('userId'); 
   const [arrayUsersLiked, setUserLiked] = useState(post.usersLiked); 
   const [liked, setLiked] = useState(arrayUsersLiked.includes(userId));  
-  const [likedNb, setLikedNb] = useState(post.usersLiked.length);  
 
   const like = (e) => { 
-   
     axios(
       { 
         method: "post", 
@@ -24,40 +79,31 @@ function LikeButton({ post }) {
         if(!liked){
           let temp = [...arrayUsersLiked];
           temp.push(userId);
-          // console.log(temp);
-          // console.log(userId);
           setUserLiked(temp);
           setLiked(true); 
         }else{  
           let newArray = arrayUsersLiked.filter(en => en !== userId);
-          // console.log(arrayUsersLiked);
           setUserLiked(newArray);
           setLiked(false);  
         }
-        setLikedNb(res.data.nbLike);  
-       
-        
       })
+      .catch(error => console.error(error));
   }  
 
   useEffect(() => {
-    if(post.usersLiked.length > 0){
-      setUserLiked(post.usersLiked); 
-    }else{
-      setUserLiked([]);
-    }
-    // console.log(liked);
-  }, []);
+    setUserLiked(post.usersLiked); 
+  }, [post.usersLiked]);
  
 
   return (
     <div className="blocLike">
       <div className={ `btn-like` } >
-          <FontAwesomeIcon icon={faHeart} id={liked ? 'like' : 'unlike'} className={liked ? 'liked' : ''} type="button" onClick={(e) => like(e)} value={likedNb} />
+          <FontAwesomeIcon icon={faHeart} id={liked ? 'like' : 'unlike'} className={liked ? 'liked' : ''} type="button" onClick={(e) => like(e)} value={arrayUsersLiked.length} />
       </div> 
-      <span className="number">{likedNb}</span>
+      <span className="number">{arrayUsersLiked.length}</span>
     </div>
   );
 }
 
 export default LikeButton;
+
